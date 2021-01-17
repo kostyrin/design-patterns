659df29f3153d467b3d7c8b706976b6b9c8babfb
Andrei Kostyrin <kostyrin2@gmail.com>
2021-01-17 21:58:04 +0200

    Add Composite.NeuronNetwork

---
+++ Composite.NeuronNetwork/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Composite.NeuronNetwork
+{
+    public static class NeuronExtension
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+
+            foreach (var from in self)
+            foreach (var to in other)
+            {
+                from.Out.Add(to);
+                to.In.Add(from);
+            }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public List<Neuron> In, Out;
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+
+            neuron1.ConnectTo(neuron2);
+
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+
+            neuron1.ConnectTo(layer1);
+            layer1.ConnectTo(layer2);
+        }
+    }
+}
