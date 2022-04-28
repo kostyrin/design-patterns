d99b84f418e4974b27600425873bf004c4332352
Andrei Kostyrin <kostyrin2@gmail.com>
2022-04-28 11:45:48 +0200

    CompositeProxySoAOrAoS

---
+++ Proxy/CompositeProxySoAOrAoS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.Aos
+{
+    class Creature
+    {
+        public byte Age { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Creatures
+    {
+        private readonly int size;
+        private readonly byte[] age;
+        private readonly int[] x, y;
+
+        public Creatures(int size)
+        {
+            this.size = size;
+            age = new byte[size];
+            x = new int[size];
+            y = new int[size];
+        }
+
+        public struct CreatureProxy
+        {
+            private readonly Creatures creatures;
+            private readonly int index;
+
+            public CreatureProxy(Creatures creatures, int index)
+            {
+                this.creatures = creatures;
+                this.index = index;
+            }
+
+            public ref byte Age => ref creatures.age[index];
+            public ref int X => ref creatures.x[index];
+            public ref int Y => ref creatures.y[index];
+        }
+
+        public IEnumerator<CreatureProxy> GetEnumerator()
+        {
+            for (int pos = 0; pos < size; ++pos)
+                yield return new CreatureProxy(this, pos);
+        }
+
+        
+    }
+}
--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Proxy.Aos;
 
 namespace Proxy
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //AoS
+            var creatures = new Creature[100];
+            foreach (var item in creatures)
+            {
+                item.X++;
+            }
+
+            //SoA
+            var creatures2 = new Creatures(100);
+            foreach (Creatures.CreatureProxy item in creatures2)
+            {
+                item.X++;
+            }
         }
     }
 }
