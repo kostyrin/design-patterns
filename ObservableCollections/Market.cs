c5c3f95645c67246e5ec5bb634d21acfdb074958
Andrei Kostyrin <a.kostyrin@sweedpos.com>
2024-02-29 19:32:00 +0200

    ObservableCollections

---
+++ ObservableCollections/Market.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollections;
+public class Market
+{
+    public Market()
+    {
+        
+    }
+
+    public BindingList<decimal> Prices = new BindingList<decimal>();
+
+    public void AddPrice(decimal price)
+    {
+        Prices.Add(price); 
+    }
+}
---
+++ ObservableCollections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace ObservableCollections;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var market = new Market();
+        market.Prices.ListChanged += (sender, eventArgs) =>
+        {
+            if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
+            {
+                Console.WriteLine($"price has been added {((BindingList<decimal>)sender)[eventArgs.NewIndex]}");
+            }
+            
+        };
+
+        market.AddPrice(100);
+        Console.ReadLine();
+    }
+}
